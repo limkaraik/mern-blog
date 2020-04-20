fd54910e924e14ca8484eac8030ade2e0d24279b
Kar Aik Lim <limkaraik@hotmail.com>
2020-04-20 18:57:12 +0800

    added logout

--- index.js
+++ index.js
@@ -7,28 +7,66 @@ const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require('./model/user');
+const {auth} = require('./middleware/auth');
 
 mongoose.connect(config.mongoURI, 
-                {useNewUrlParser: true}).then(() =>console.log('DB connected'))
+                {useNewUrlParser: true, useUnifiedTopology: true}).then(() =>console.log('DB connected'))
                                         .catch(err =>console.error(err));
 
-// app.get('/', (req, res) => res.send('Hello World!'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    res.send("yo waddup mate")
+app.get('/api/user/auth',auth, (req,res)=>{
+    res.status(200).json({
+        _id: req.token,
+        isAuth: true,
+        email: req.user.email,
+        name : req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
 });
 
 app.post('/api/users/register', (req,res) => {
     const user = new User(req.body)
 
-    user.save((err, userData)=> {
-        if (err) return res.json ({success: false, err})
+    user.save((err, doc)=> {
+        if (err) return res.json ({success: false, err});
+        res.status(200).json({success: true, userData: doc});
+    })
+})
+
+app.post('/api/user/login', (req,res)=>{
+    //find email
+    User.findOne({email: req.body.email}, (err, user)=>{
+        if (!user) return res.json({loginSuccess: false, message: "Incorrect email"});
+
+        //if email found, find password
+        user.comparePassword(req.body.password, (err, isMatch) =>{
+            if (!isMatch) return res.json({loginSuccess: false, message: "Incorrect Password"});
+        });
+
+        //generate token
+        user.generateToken((err,user)=>{
+        if (err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token).status(200).json({
+            loginSuccess: true
+        })
     })
 
-    return res.status(200).json({success: true})
+    })
+
 })
+
+app.get('/api/user/logout', auth, (req,res)=>{
+    User.findOneAndUpdate({ _id: req.user._id}, {token:""}, (err)=>{
+        if (err) return res.json({ success: false, err});
+        return res.status(200).send({
+            success: true
+        })
+    })
+})
+
 
 app.listen(5000);
