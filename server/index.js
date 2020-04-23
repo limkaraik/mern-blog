80ff54b4a2f6255d8e3af1cbe67f5c7c61a549d0
Kar Aik Lim <limkaraik@hotmail.com>
2020-04-23 17:25:17 +0800

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -68,6 +68,16 @@ app.get('/api/users/logout', auth, (req,res)=>{
     })
 })
 
+
+if (process.env.NODE_ENV === 'production'){
+
+    app.use(express.static("client/build")
+    
+    app.get("*", (req,res)=> {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    }))
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`Server Running at ${port}`)
